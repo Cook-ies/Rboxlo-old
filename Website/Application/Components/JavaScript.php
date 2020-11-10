73b4bb9befc5a83ad5b20f63c5a2ca509f8ba08b
lightbulblighter <59720715+lighterlightbulb@users.noreply.github.com>
2020-11-10 17:58:20 -0600

    Fix tooltips
    We have to use an older version of popper compatible with Bootstrap 4.3.1. Perhaps we should upgrade MDBootstrap or Bootstrap soon?

--- Website/Application/Components/JavaScript.php
+++ Website/Application/Components/JavaScript.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.3.3/umd/popper.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
 <script type="text/javascript" src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="<?= get_server_host() ?>/html/js/mdb.min.js"></script>
 <script type="text/javascript" src="<?= get_server_host() ?>/html/js/mdb-plugins-gathered.min.js"></script>
